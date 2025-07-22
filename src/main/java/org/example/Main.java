556d1473e5b542ff98ee48447e1f33373b7ba3d1
Brendan Dileo <dileob23@gmail.com>
2025-07-22 10:14:17 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start Main");
+        CallStackMethodInvoc.methodA(3);
+        System.out.println("End Main");
+    }
+}
